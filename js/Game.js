10759056c0d2fc5c55ab16b97987bc3cee1be0e7
Pavel <u.pavel03@gmail.com>
2020-12-11 22:46:05 +0700

    Подготовка к реализации волн

--- js/Game.js
+++ js/Game.js
@@ -4,22 +4,36 @@ class Game
     {
         this.app = new PIXI.Application({
             view: document.getElementById("game-canvas"),
-            resizeTo: window,
-            backgroundColor: 0x1099bb
+            resizeTo: window, // привязка размера canvas к размеру окна
         });
+        
+        this.screenCenter = new Vector2(0, 0);
 
-        this.backgroundSprite = new PIXI.TilingSprite.from('img/sand-background.png', {});
-        this.displacementSprite = new PIXI.Sprite.from('img/base_displacement_map.png');
-        this.displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-        this.displacementFilter = new PIXI.filters.DisplacementFilter(this.displacementSprite);
-
+        this.backgroundSprite = new PIXI.TilingSprite.from('img/background.png', {});
         this.app.stage.addChild(this.backgroundSprite);
-        this.app.stage.addChild(this.displacementSprite);
-        this.backgroundSprite.filters = [this.displacementFilter];
 
+        this.wavesSprite = new PIXI.Sprite(); // displacement map волн
+        this.wavesBackground = new PIXI.Graphics(); // черный фон 
+        this.wavesSprite.addChild(this.wavesBackground);
+        this.app.stage.addChild(this.wavesSprite);
 
+        // эффект искажения для воды
+        this.displacementSprite1 = new PIXI.Sprite.from('img/base_displacement_map.png');
+        this.displacementSprite1.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+        this.displacementFilter1 = new PIXI.filters.DisplacementFilter(this.displacementSprite1);
+        this.app.stage.addChild(this.displacementSprite1);
+
+        this.displacementSprite2 = new PIXI.Sprite.from('img/base_displacement_map.png');
+        this.displacementSprite2.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+        this.displacementFilter2 = new PIXI.filters.DisplacementFilter(this.displacementSprite2);
+        this.app.stage.addChild(this.displacementSprite2);
+        this.displacementSprite2.x = 100;
+
+        this.backgroundSprite.filters = [this.displacementFilter1, this.displacementFilter2];
 
         this.resize();
+        
+        this.createWave();
 
         //запускаем игровой цикл
         this.app.ticker.add(delta => this.gameLoop(delta));
@@ -29,9 +43,19 @@ class Game
     {
         window.scrollTo(0, 0);
 
+        //получение нового размера окна
         let width = window.innerWidth || document.body.clientWidth; 
         let height = window.innerHeight || document.body.clientHeight;
+
+        this.screenCenter.x = width / 2;
+        this.screenCenter.y = height / 2;
         
+        //изменение размера черного фона волн
+        this.wavesBackground.clear();
+        this.wavesBackground.beginFill(0x000000, 0.5);
+        this.wavesBackground.drawRect(-1, -1, width + 2, height + 2);
+
+        //изменение размера фона
         this.backgroundSprite.width = width;
         this.backgroundSprite.height = height;
 
@@ -39,8 +63,27 @@ class Game
 
     gameLoop(delta)
     {
-        this.displacementSprite.x += 0.7 * delta;
-        this.displacementSprite.y += 0.7 * delta;
-        //this.background_sprite.rotation += 0.01 * delta;
+        this.displacementSprite1.x += 0.6 * delta;
+        this.displacementSprite1.y += 0.25 * delta;
+        this.displacementSprite2.x -= 0.15 * delta;
+        this.displacementSprite2.y -= 0.05 * delta;
+
+        //this.wavesSprite.scale.x += 0.01 * delta;
+        //this.wavesSprite.scale.y += 0.01 * delta;
+    }
+
+    createWave()
+    {
+        // this.graphics = new PIXI.Graphics();
+        // this.graphics.lineStyle(10, 0xFFBD01, 1);
+        // this.graphics.beginFill(0xC34288, 1);
+        // this.graphics.drawCircle(400, 250, 50);
+        // this.graphics.endFill();
+
+        
+        //this.waveSprite.addChild(this.graphics);
+        //this.wavesSprite.anchor.set(0.5);
+        // this.waveSprite.scale.x = 0;
+        // this.waveSprite.scale.y = 0;       
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 let g_game;
 
+function Vector2(x, y)
+{
+    this.x = x != null ? x : 0;
+    this.y = y != null ? y : 0;
+}
+
 window.onload = function()
 {
     g_game = new Game();
